3467abcaffa9f07f6ab5ef0f004d11c400d0582c
chetanraj99 <chetanrajpatel99@gmail.com>
2024-03-04 20:53:56 +0530

    api

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -2,34 +2,37 @@ import axios from "axios";
 import React, { createContext, useEffect, useState } from "react";
 export const GlobalContext = createContext();
 const ContextProvider = ({ children }) => {
-	const [loading, setLoading] = useState(false);
-	const [loggedIn, setLoggedIn] = useState(true);
-	const [employeeList, setEmployeeList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [employeeList, setEmployeeList] = useState([]);
 
-	useEffect(() => {
-		const getEmployeeList = async () => {
-			try {
-				const { data } = axios.get("http://localhost:8080/employee/employees");
-				setEmployeeList(data);
-			} catch (error) {}
-		};
-		getEmployeeList();
-	}, [employeeList]);
+  useEffect(() => {
+    const getEmployeeList = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:8080/employee/employees"
+        );
+        setEmployeeList(data);
+        console.log(data);
+      } catch (error) {}
+    };
+    getEmployeeList();
+  }, []);
 
-	return (
-		<GlobalContext.Provider
-			value={{
-				loading,
-				setLoading,
-				loggedIn,
-				setLoggedIn,
-				employeeList,
-				setEmployeeList,
-			}}
-		>
-			{children}
-		</GlobalContext.Provider>
-	);
+  return (
+    <GlobalContext.Provider
+      value={{
+        loading,
+        setLoading,
+        loggedIn,
+        setLoggedIn,
+        employeeList,
+        setEmployeeList,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 };
 
 export default ContextProvider;
