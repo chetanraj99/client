ceda30907f8f62fa28c4900bb59cf737122fac05
rpc1752 <rpc1752@gmail.com>
2024-03-10 08:44:29 +0530

    updated

--- src/page/CreateEmployee.jsx
+++ src/page/CreateEmployee.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useState, useTransition } from "react";
+import { useContext, useState } from "react";
 import Loader from "../components/Loader";
 import baseURL from "../api/axiosApi";
 import { GlobalContext } from "../context/ContextProvider";
